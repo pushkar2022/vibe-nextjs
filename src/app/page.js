2f3268e3c7c2753dcdccf8f3c586e0bf877c6a6d
harshit <harshit@dev.familyone.io>
2025-07-22 07:12:55 +0530

    kljhk

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,37 @@
 "use client";
 
+import { Input } from '../components/ui/input';
+// import { Input } from '@/components/ui/input';
 import { Button } from '../components/ui/button';
-import React from 'react';
+import React, { useState } from 'react';
 
 function Page() {
-  const handleClick = () => {
-    alert('Button clicked!');
-  };
+  const [inputs,setInputs]=useState('')
+  // const handleClick = () => {
+  //   alert(`Button clicked!${inputs}`);
+  // };
 
+    const handleClick = async () => {
+    try {
+      const res = await fetch('/api/inngest', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email: inputs }),
+      });
+
+      const data = await res.json();
+      alert(`Event Triggered! ${JSON.stringify(data)}`);
+    } catch (error) {
+      console.error('Failed to trigger function', error);
+    }
+  };
   return (
-    <div>
-      <Button onClick={handleClick} variant="default">
-        Click Me
+    <div className="p-4 max-w-7xl mx-auto">
+      <Input value={inputs} onChange={(e)=>setInputs(e.target.value)}/>
+      <Button onClick={handleClick} >
+        Invoke Background 
       </Button>
     </div>
   );
